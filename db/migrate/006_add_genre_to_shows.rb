a791dedd94f2b541a352304c3a28faf3734bca30
Josh Cesana <joshuacesana@gmail.com>
2018-07-19 02:12:38 +0000

    automatically backed up by learn

--- db/migrate/006_add_genre_to_shows.rb
+++ db/migrate/006_add_genre_to_shows.rb
@@ -1,5 +1,5 @@
-class AddGenreToShow < ActiveRecord::Migration[4.2]
+class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :network_id, :integer
+    add_column :shows, :genre, :string
   end
 end
